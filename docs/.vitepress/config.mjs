c3b1d3fde673adef527bcf5ea93b84f2349e1764
ILICHI <yunrulin0@gmail.com>
2025-01-30 13:50:34 +0800

    112

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress' // 导入VitePress的配置函数
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+    base: '/personal-blog/',
     title: "伊莉奇博客站", // 设置网站的标题
     description: "伊莉奇的技术博客存放地", // 设置网站的描述
     themeConfig: {
